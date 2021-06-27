2f2f7eefe74df5aa8de2a05045c0bf0b6d6fa0e3
LukaszPasich <lukas.zed81@gmail.com>
2021-06-27 10:32:53 +0000

    STYLE: Add styling and fade out action to Flash messages

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,28 @@ $(document).ready(function () {
     $('.sidenav').sidenav({edge: "right"});
 });
 
-// Close Flash messages
-$("#close-message").on("click", closeFlash);
 
-function closeFlash(){
-    $("#flash-message").css("display", "none");
-    return false;
-}
+// // Close Flash messages
+// $("#close-message").on("click", closeFlash);
+
+// function closeFlash(){
+//     $("#flash-message").css("display", "none");
+//     return false;
+// }
+
+
+$(document).ready( function() {
+    setTimeout(function () {
+        $("#flash-message")
+            .animate({
+                'opacity': 0
+            }, 750); //you can set a speed just like fadeOut() or fadeIn()
+    },5000);
+
+    // $("#div").hover(function(){
+    //     $("#div").css("opacity","1");
+    // },function(){
+    //     $("#div").css("opacity","0");
+    // });
+
+});
