9b9c2960ccb4e3e237e594c11b4261d28253e21a
LukaszPasich <lukas.zed81@gmail.com>
2021-06-30 23:23:21 +0000

    FEAT: Add Session User Recipes

--- app.py
+++ app.py
@@ -53,6 +53,7 @@ def signup():
         # put the new user into 'session cookie'
         session["user"] = request.form.get("username").lower()
         flash("You have signed up successfully!")
+        return redirect(url_for("my_recipes", username=session["user"]))
 
     return render_template("signup.html")
 
@@ -71,6 +72,8 @@ def login():
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}!".format(request.form.get("username")))
+                return redirect(url_for(
+                        "my_recipes", username=session["user"]))
             else:
                 # invalid password match
                 flash("Incorrect username and/or password")
@@ -85,10 +88,13 @@ def login():
 
 
 # ---------- MY RECIPES PAGE ----------
-@app.route("/my_recipes")
-def my_recipes():
-    return render_template("myrecipes.html")
-    
+@app.route("/my_recipes/<username>", methods=["GET", "POST"])
+def my_recipes(username):
+    # grab the session user's username from db
+    username = mongo.db.users.find_one(
+            {"username": session["user"]})["username"]
+    return render_template("myrecipes.html", username=username)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
