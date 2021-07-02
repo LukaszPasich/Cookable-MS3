7f6d6fe9493a60725170f87c80255068f1af706e
LukaszPasich <lukas.zed81@gmail.com>
2021-07-02 20:42:35 +0000

    FEAT: Add Add Recipe template and initial input fields

--- app.py
+++ app.py
@@ -109,6 +109,12 @@ def logout():
     return redirect(url_for("login"))
 
 
+# ---------- ADD RECIPE - C(RUD) ----------
+@app.route("/add_recipe")
+def add_recipe():
+    return render_template("add_recipe.html")
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
