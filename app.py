923c2154298cf59aa642e4b07a0eb2b2995da234
LukaszPasich <lukas.zed81@gmail.com>
2021-07-10 22:43:34 +0000

    FEAT: Add functionality for placing advert where certain keywords exist in recipes

--- app.py
+++ app.py
@@ -34,13 +34,11 @@ def get_recipes():
 # ---------- SEARCH ----------
 @app.route("/search", methods=["GET", "POST"])
 def search():
-    username = mongo.db.users.find_one(
-            {"username": session["user"]})["username"]
     search = request.form.get("search")
     recipes = list(mongo.db.recipes.find({"$text": {"$search": search}}))
     recipesearched = list(mongo.db.recipes.find())
     return render_template("recipes.html", recipes=recipes,
-                           recipesearched=recipesearched, username=username)
+                           recipesearched=recipesearched)
 
 
 # ---------- SIGN UP ----------
@@ -200,11 +198,26 @@ def full_recipe(recipe_id):
 
     fullrecipes = mongo.db.recipes.find({"_id": ObjectId(recipe_id)})
 
+    # gets all records with the keyword
+    
+    adsearch = list(mongo.db.recipes.find({"$text": {"$search": "dog"}}))
+    
+    # gets the list of only _id: value pairs of those records
+    # https://stackoverflow.com/questions/13254241/removing-key-values-pairs-from-a-list-of-dictionaries
+    idvalues = [{k: v for k, v in d.items() if k == '_id'} for d in adsearch]
+    
+    # turns the list of dicts into the list of "_id" values
+    # https://stackoverflow.com/questions/20891141/convert-list-of-dicts-to-list
+    adverts = []
+    for idvalue in idvalues:
+        adverts.append(idvalue["_id"])
+
     # ingredients = mongo.db.users.find_one(
     #     {request.form.get("ingredients").split("\r\n")})
 
     return render_template("full_recipe.html",
-                           recipe=recipe, fullrecipes=fullrecipes)
+                           recipe=recipe, fullrecipes=fullrecipes,
+                           adverts=adverts)
 
     # ingredients=ingredients)
 
