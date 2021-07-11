5c49c77a3ece9488f1d1a44d517a76891e57fdad
LukaszPasich <lukas.zed81@gmail.com>
2021-07-11 15:48:27 +0000

    FEAT: Functionality for Send contact form from contact page with Success/Fail responses

--- static/js/sendEmail.js
+++ static/js/sendEmail.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("service_611qf0k", "cookable", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.email.value,
+        "cookable_request": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+            flashEmail = document.getElementById("flash-messages-email");
+            flashEmail.style.display = 'inline-block';
+            flashEmail.innerHTML = "Message sent successfully";
+        },
+        function(error) {
+            console.log("FAILED", error);
+            flashEmail = document.getElementById("flash-messages-email");
+            flashEmail.style.display = 'inline-block';
+            flashEmail.innerHTML = "Message not sent!";
+        }
+    );
+    setTimeout(function(){window.location.reload();},3500);
+    return false;
+}
